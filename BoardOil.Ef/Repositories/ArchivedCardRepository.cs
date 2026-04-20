26dcb3fb28cd4015ec6c66d614ec26db0f7dc810
Luke <luke@savagexonclave.com>
2026-04-20 21:03:31 +0100

    Include archive.json in board package export/import

--- BoardOil.Ef/Repositories/ArchivedCardRepository.cs
+++ BoardOil.Ef/Repositories/ArchivedCardRepository.cs
@@ -8,6 +8,8 @@ namespace BoardOil.Ef.Repositories;
 public sealed class ArchivedCardRepository(IAmbientDbContextLocator ambientDbContextLocator)
     : RepositoryBase<EntityArchivedCard>(ambientDbContextLocator), IArchivedCardRepository
 {
+    private const int MaxOriginalCardIdsPerQuery = 500;
+
     public async Task<IReadOnlyList<EntityArchivedCard>> ListByBoardAsync(int boardId, string? normalisedSearch, int offset, int limit)
     {
         var query = BuildQuery(boardId, normalisedSearch);
@@ -22,6 +24,50 @@ public sealed class ArchivedCardRepository(IAmbientDbContextLocator ambientDbCon
 
     public Task<int> CountByBoardAsync(int boardId, string? normalisedSearch) =>
         BuildQuery(boardId, normalisedSearch).CountAsync();
+
+    public async Task<IReadOnlyList<EntityArchivedCard>> ListForExportAsync(int boardId) =>
+        await DbSet
+            .AsNoTracking()
+            .Where(x => x.BoardId == boardId)
+            .OrderByDescending(x => x.ArchivedAtUtc)
+            .ThenByDescending(x => x.Id)
+            .ToListAsync();
+
+    public async Task<IReadOnlyList<int>> ListExistingOriginalCardIdsAsync(IReadOnlyList<int> originalCardIds)
+    {
+        if (originalCardIds.Count == 0)
+        {
+            return [];
+        }
+
+        var uniqueOriginalCardIds = originalCardIds
+            .Distinct()
+            .ToArray();
+        var existingOriginalCardIds = new HashSet<int>();
+
+        foreach (var idChunk in uniqueOriginalCardIds.Chunk(MaxOriginalCardIdsPerQuery))
+        {
+            var chunk = idChunk.ToArray();
+            var existingIdsInChunk = await DbSet
+                .AsNoTracking()
+                .Where(x => chunk.Contains(x.OriginalCardId))
+                .Select(x => x.OriginalCardId)
+                .Distinct()
+                .ToListAsync();
+            foreach (var existingId in existingIdsInChunk)
+            {
+                existingOriginalCardIds.Add(existingId);
+            }
+        }
+
+        return existingOriginalCardIds.ToList();
+    }
+
+    public Task<int?> GetMinimumOriginalCardIdAsync() =>
+        DbSet
+            .AsNoTracking()
+            .Select(x => (int?)x.OriginalCardId)
+            .MinAsync();
 
     public Task<EntityArchivedCard?> GetByIdAsync(int boardId, int archivedCardId) =>
         DbSet
